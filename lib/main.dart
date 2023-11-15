16b76d3df5e302964e97b7e8fbf55c56eb7eefa2
mantisy <mantisy@mantisys-Mac-mini.local>
2023-11-15 17:28:34 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,85 @@
-// ignore_for_file: use_build_context_synchronously
-
-import 'package:biometric_storage/pagetwo.dart';
-import 'package:biometric_storage/utils/authentication.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screen_lock/flutter_screen_lock.dart';
+import 'package:local_auth/local_auth.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Example',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const AuthenticationPage(),
+      home: const MyHomePage(),
     );
   }
 }
 
-class AuthenticationPage extends StatefulWidget {
-  const AuthenticationPage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
   @override
-  State<AuthenticationPage> createState() => _AuthenticationPageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _AuthenticationPageState extends State<AuthenticationPage> {
+class _MyHomePageState extends State<MyHomePage> {
+  Future<void> localAuth(BuildContext context) async {
+    final localAuth = LocalAuthentication();
+    final didAuthenticate = await localAuth.authenticate(
+        localizedReason: 'Comfirm fingerprint to continue',
+        options: const AuthenticationOptions(
+          biometricOnly: true,
+        ));
+    if (didAuthenticate) {
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      body: SafeArea(
-          child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              'Login',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 38,
-                fontWeight: FontWeight.w800,
-              ),
+      appBar: AppBar(
+        title: const Text('Next Screen Lock'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: 700,
             ),
-            const SizedBox(
-              height: 28,
+            child: Wrap(
+              spacing: 16,
+              runSpacing: 16,
+              alignment: WrapAlignment.start,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    screenLock(
+                      context: context,
+                      correctString: '1234',
+                      customizedButtonChild: const Icon(
+                        Icons.fingerprint,
+                      ),
+                      customizedButtonTap: () async => await localAuth(context),
+                      onOpened: () async => await localAuth(context),
+                    );
+                  },
+                  child: const Text(
+                    'use local_auth \n(Show local_auth when opened)',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
             ),
-            const Text(
-              ' use your fingerprint to log  into the app ',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            const SizedBox(
-              height: 28,
-            ),
-            const Divider(
-              color: Colors.white60,
-            ),
-            const SizedBox(
-              height: 28,
-            ),
-            ElevatedButton.icon(
-              onPressed: () async {
-                bool auth = await Authentication.authentication();
-                print('can authentication: $auth');
-                if (auth) {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const SecondPage(),
-                      ));
-                }
-              },
-              icon: IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.fingerprint),
-              ),
-              label: const Text('Authentication'),
-            )
-          ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
